29e25b64c6d8351bbe5d2d40ab0a28f1789606cf
zza <b23040218@njupt.edu.cn>
2025-03-29 13:32:16 +0800

    111

--- app/product/biz/service/update_stock.go
+++ app/product/biz/service/update_stock.go
@@ -15,6 +15,6 @@ func NewUpdateStockService(ctx context.Context) *UpdateStockService {
 // Run create note info
 func (s *UpdateStockService) Run(req *product.UpdateStockRequest) (resp *product.UpdateStockResponse, err error) {
 	// Finish your business logic.
-	resp := &product.UpdateStockResponse{}
+	resp = &product.UpdateStockResponse{}
 	return
 }
